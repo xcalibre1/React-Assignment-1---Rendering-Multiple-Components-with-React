e538da02205d337a6c22e9926799b55a95c47d02
Yash Prakash Mishra <34412462+xcalibre1@users.noreply.github.com>
2021-01-29 00:30:48 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+    <h1 data-ns-test="project-name">contractor-friend</h1>
+    <h1 data-ns-test="project-description">its a project in which customer can select contractor on the basis rating of constructor</h1>
+    </div>
   )
 }
 
